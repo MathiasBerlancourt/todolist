ddb660fbc86385d5e9a434c4e07d684b47b39486
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-05-12 17:17:52 +0200

    tailwind fix

--- frontend/src/assets/components/Todo.tsx
+++ frontend/src/assets/components/Todo.tsx
@@ -1,9 +1,11 @@
 export const Todo = ({ _id, name, description }) => {
   return (
-    <div>
-      <h2>{name}</h2>
+    <div className="flex flex-col-reverse">
+      <div>
+        <h2 className="text-red-500">{name}</h2>
 
-      <p>{description}</p>
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,29 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-
-  extend: {
-    theme: {
-      colors: {
-        gold: "#f2dfa5",
-        blue: "#526ACD",
-        black: "#000000",
-        white: "#FFFFFF",
-        orange: "#ff7849",
-        green: "#13ce66",
-        yellow: "#ffc82c",
-        "gray-dark": "#273444",
-        gray: "#8492a6",
-        graay: "#464646",
-        "gray-light": "#d3dce6",
-        salmon: "salmon",
-        dark: "rgb(15, 15, 15)",
-        red: "#ff0000",
-      },
-      fontFamily: {
-        oxygen: ["Oxygen", "sans-serif"],
-      },
-    },
+  content: ["*.{html,js,jsx,tsx}"],
+  theme: {
+    extend: {},
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [],
 };
